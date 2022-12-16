c28534dc04a645cc6992322bfd674e3c049c3f93
yoann-fermaud <yoann.fermaud@laplateforme.io>
2022-12-16 20:02:36 +0100

    Update Web project

--- JS/earth.js
+++ JS/earth.js
@@ -21,14 +21,10 @@ let day = false;     // DayCheckVar
 function main()
 {
     // Select Canvas in HTML and give it load animation //
-
     let canv = document.querySelector('canvas');
     canv.classList.add("load");
 
-    // ------------------------------------------------ //
-
     // Hold Mouse Check //
-
     addEventListener('mousedown', () => {
         hold = true
         return hold
@@ -46,118 +42,33 @@ function main()
     // ----------------- //
 
     if (1025 <= screen.width) {
-        // Setup Canvas and Camera for Render //
-
-        const canvas = document.querySelector('#c');
-
-        camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-
-        camera.position.x = 0;
-        camera.position.y = 0;
-        camera.position.z = 10;
-        scene = new THREE.Scene();
-        scene.add(camera);
-
-        renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setPixelRatio(window.devicePixelRatio);
-
-        renderer.autoClear = false;
-        renderer.setClearColor(0x00000, 0.0); 
+         earth(50, 1, 1);
     }else if (769 <= screen.width <= 1024) {
-        // Setup Canvas and Camera for Render //
-
-        const canvas = document.querySelector('#c');
-
-        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-
-        camera.position.x = 0;
-        camera.position.y = 0;
-        camera.position.z = 10;
-        scene = new THREE.Scene();
-        scene.add(camera);
-
-        renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
-
-        renderer.setSize(window.innerWidth/1.8, window.innerHeight/1.8);
-        renderer.setPixelRatio(window.devicePixelRatio);
-
-        renderer.autoClear = false;
-        renderer.setClearColor(0x00000, 0.0); 
+        earth(60, 1.8, 1.8); 
     }else if(481 <= screen.width <= 768){
-        // Setup Canvas and Camera for Render //
-
-        const canvas = document.querySelector('#c');
-
-        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-
-        camera.position.x = 0;
-        camera.position.y = 0;
-        camera.position.z = 10;
-        scene = new THREE.Scene();
-        scene.add(camera);
-
-        renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
-
-        renderer.setSize(window.innerWidth/2.2, window.innerHeight/2.2);
-        renderer.setPixelRatio(window.devicePixelRatio);
-
-        renderer.autoClear = false;
-        renderer.setClearColor(0x00000, 0.0); 
+        earth(70, 2.2, 2.2);
     }else if(320 <= screen.width <= 480){
-        // Setup Canvas and Camera for Render //
-
-        const canvas = document.querySelector('#c');
-
-        camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1000);
-
-        camera.position.x = 0;
-        camera.position.y = 0;
-        camera.position.z = 10;
-        scene = new THREE.Scene();
-        scene.add(camera);
-
-        renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
-
-        renderer.setSize(window.innerWidth/2.8, window.innerHeight/2.8);
-        renderer.setPixelRatio(window.devicePixelRatio);
-
-        renderer.autoClear = false;
-        renderer.setClearColor(0x00000, 0.0); 
+        earth(80, 2.8, 2.8);
     }
 
-
-
-    // ----------------------------------- //
-
-
     // Create Earth //
-
     const earthgeometry = new THREE.SphereGeometry(earth_scale,256,256);
-
     const earthmaterial = new THREE.MeshPhongMaterial({});
-
     const earthmesh = new THREE.Mesh(earthgeometry,earthmaterial);
 
     scene.add(earthmesh);
 
     // ----------- //
-
     const ambientlight = new THREE.AmbientLight(0xffffff, 0.4);
     scene.add(ambientlight);
 
     // Create point light And Define his position //
-
     const pointerlight = new THREE.PointLight(0xffffff,1.4);
 
     pointerlight.position.set(5,3,5);
     scene.add(pointerlight);
 
-    //------------------------------------------ //
-
     // Create Cloud //
-    
     const cloudgeometry =  new THREE.SphereGeometry(earth_scale + 0.1 ,32,32);
 
     const cloudmaterial = new THREE.MeshPhongMaterial({
@@ -169,10 +80,7 @@ function main()
     const cloudmesh = new THREE.Mesh(cloudgeometry,cloudmaterial);
     scene.add(cloudmesh);
 
-    // ------------ //
-
     // Create Stars //
-
         const stargeometry =  new THREE.SphereGeometry(64,64,64);
 
         const starmaterial = new THREE.MeshBasicMaterial({
@@ -187,67 +95,47 @@ function main()
     // ------------ //
 
     // Mouse Position //
-
     const mouse = {
         x: undefined,
         y: undefined
     }
-
-    // -------------- //
-
     // _____________________________________________/ Animate Function \_____________________________________________ //
 
     const animate = () =>{
-
-        
-        // renderer.setSize(window.innerWidth, window.innerHeight);
-        // renderer.setPixelRatio(window.devicePixelRatio);
         
         // Request Every Frame To WebBrowser //
-
         requestAnimationFrame(animate);
 
-        // --------------------------------- //
-        
         // Star Mouvement //
-
         starmesh.rotation.z += 0.0005;
 
         // -------------- //
 
-        
         if(zoom == false) {
 
             // Mouse Earth Movement //
-
             if (hold == true) {
                 if (mouse.x >= -0.5 && mouse.x <= 0.5) {
                     earthmesh.rotation.y -= mouse.x / 20;
                     cloudmesh.rotation.y += mouse.x / 20;
                     lastmousepos = mouse.x
-                    
+
                     if(earthmesh.rotation.y > Math.PI*2){
                         earthmesh.rotation.y = 0;
                     }
-
                     if(earthmesh.rotation.y < 0){
                         earthmesh.rotation.y = Math.PI*2;
                     }
                 }
-
                 else{
                     hold = false
                 }
             }
 
-            // -------------------- //
-
             // Automatic Earth Movement //
-
             else{
 
                 // Change cloud Opacity depend on day or night //
-
                 if(day){
                     cloudmaterial.opacity = 1;
                 }
@@ -255,18 +143,12 @@ function main()
                     cloudmaterial.opacity = 0.4;
                 }
 
-                // ------------------------------------------- //
-
                 // Page Load Camera Zoom //
-
                 if( camera.position.z > 2.1){
                     camera.position.z -= 0.06;
                 }
 
-                // --------------------- //
-
                 // Show Every Element On Screen //
-
                 else{
                     if(intro == false)
                     {
@@ -278,8 +160,6 @@ function main()
                         intro = true;
                     }
                 }
-
-                // ----------------------------- //
 
                 // __________________________________/Night Mode\__________________________________ //
 
@@ -343,11 +223,9 @@ function main()
                         html.classList.remove("dark")
                     }, 500);
                 }
-
                 // __________________________________/Night Mode\__________________________________ //
                
                 // Check If Nav-Bar Out //
-
                 if(nav == true){
                     if( camera.position.x <= 0.5){
                         camera.position.x += 0.01;
@@ -369,22 +247,15 @@ function main()
                     night.classList.remove("nav");
                 }
 
-                // ---------------------- //
-
                 // Replace Earth and Camera after a Zoom //
-
                 if( camera.position.z <= 2){
                     camera.position.z += 0.02;
                 }  
-
                 if( earthmesh.rotation.x >= 0){
                     earthmesh.rotation.x -= 0.01;
                 }
 
-                // ------------------------------------ //
-
                 // Idle Animation Depend of Last rotation //
-
                 if(lastmousepos < 0){
                     earthmesh.rotation.y += 0.001;
                     cloudmesh.rotation.y -= 0.0005;
@@ -397,7 +268,6 @@ function main()
                         camera.position.z += 0.02;
                     }
                 }
-
                 else{
                     earthmesh.rotation.y -= 0.001;
                     cloudmesh.rotation.y += 0.0005;
@@ -406,26 +276,14 @@ function main()
                         earthmesh.rotation.y = Math.PI*2;
                     }
                 }
-
-                // -------------------------------------- //
-        
             }
-
-            // -------------------------- //
         }
-
         // __________________________________/Zoom Country\__________________________________ //
-
         else{
-
             // Remove Nav Bar //
-
             nav = false;
 
-            // -------------- //
-
             // Camera Center //
-
             if( camera.position.x >= 0){
                 camera.position.x -= 0.03;
             }
@@ -447,63 +305,62 @@ function main()
                             show_country_info();        // Show Country Window
                         }
                     }
-                    
                     else{
                         camera.position.z -= 0.03;      // Camera Zoom
                     }
                 }
-                
                 else{
                     earthmesh.rotation.x += 0.04;       // Change EarthMesh Y Position
                 }
             }
-            
             else{
-
                  // Check If EarthMesh rotation is < or > To Sphere radius and if so reset it (Make Coordonates much easyer to setup, And make negative Coordonates non existant) //
-
                 if(earthmesh.rotation.y > Math.PI*2){
                     earthmesh.rotation.y = 0;
                 }
-
                 if(earthmesh.rotation.y < 0){
                     earthmesh.rotation.y = Math.PI*2;
                 }
-
                 earthmesh.rotation.y -= 0.04;   // Change EarthMesh X Position
 
-                // ------------------------------------------------------------ Can be find in the idle section too ------------------------------------------------------------ //
-                
+                // ------------------------------------------------------------ Can be find in the idle section too ------------------------------------------------------------ // 
             }
         }
-
         // __________________________________/Zoom Country\__________________________________ //
 
         // call Render Every Frame //
-
         render();
-
-        // ----------------------- //
-
     }
 
 // _____________________________________________/ Animate Function \_____________________________________________ //
 
-
     // Render Function //
-
     const render = () => {
         renderer.render(scene,camera);
     }
 
-    // --------------- //
-
     // call Animate Function //
-
     animate();
+}
 
-    // --------------------- //
+function earth(camValue, xSize, ySize){
+    // Setup Canvas and Camera for Render //
 
+    const canvas = document.querySelector('#c');
+
+    camera = new THREE.PerspectiveCamera(camValue, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 10;
+
+    scene = new THREE.Scene();
+    scene.add(camera);
+
+    renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true,});
+    renderer.setSize(window.innerWidth/xSize, window.innerHeight/ySize);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.autoClear = false;
+    renderer.setClearColor(0x00000, 0.0);
 }
 
 // Load Main Function on Load //
@@ -512,63 +369,7 @@ window.onload = main;
 
 // ------------------------- //
 
-
-
-// _____________________________________________/ Zoom In \_____________________________________________ //
-
-function zoom_in(cord_x,cord_y,coutry) 
-{
-   zoom = true;
-   zoomX = cord_x;
-   zoomY = cord_y;
-   country_name = coutry;
-
-   let night = document.querySelector('.label');
-   let info_card = document.getElementById("pop-up");
-   let info_text = document.getElementById("pop-up-text");
-   let nav = document.querySelector('.nav-btn');
-
-    // Load Country info //
-
-   info_card.style.backgroundImage = "url(../Images/Japon/" + country_name + ".jpg)";
-   info_text.innerHTML = country_name;
-
-    // ----------------- //
-
-    // Remove All screen HUD //
-
-   nav.classList.add("hide");
-   night.classList.add("hide");
-   night.classList.remove("nav");
-
-    // --------------------- //
-
-}
-
-// ____________________/ Zoom In \____________________ //
-
-
-
-
-
-
-// ____________________/ Zoom OUT \____________________ //
-
-function zoom_out() 
-{
-   zoom = false;
-   hide_country_info();
-}
-
-// ____________________/ Zoom OUT \____________________ //
-
-
-
-
-
-
 // ____________________/ Nav OUT \____________________ //
-
 function nav_out() 
 {
     if(nav == false){
@@ -578,93 +379,9 @@ function nav_out()
         nav = false;
     }
 }
-
 // ____________________/ Nav OUT \____________________ //
 
-
-
-
-
-
-// ____________________/ Show Country Info \____________________ //
-
-function show_country_info() 
-{
-    let title = document.querySelector('.ml2');
-    let info_card = document.getElementById("pop-up");
-    let canvas = document.querySelector("canvas");
-
-    title.classList.add("hide");
-    info_card.classList.add("reveal");
-    canvas.classList.add("reveal");
-}
-
-// ____________________/ Show Country Info \____________________ //
-
-
-
-
-
-
-// ____________________/ Hide Country Info \____________________ //
-
-function hide_country_info() 
-{
-    let night = document.querySelector('.label');
-    let nav = document.querySelector('.nav-btn');
-    let title = document.querySelector('.ml2');
-    let info_card = document.getElementById("pop-up");
-    let canvas = document.querySelector("canvas");
-
-    nav.classList.remove("hide");
-    title.classList.remove("hide");
-    info_card.classList.remove("reveal");
-    canvas.classList.remove("reveal");
-    night.classList.remove("hide");
-}
-
-// ____________________/ Hide Country Info \____________________ //
-
-
-
-
-
-
 // _____________________________________________/ Title Animation \_____________________________________________ //
-
-var textWrapper = document.querySelector('.ml2');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
 function titleAnim(){
     anime.timeline({loop: false})
-    .add({
-      targets: '.ml2 .letter',
-      scale: [4,1],
-      opacity: [0,1],
-      translateZ: 0,
-      easing: "easeOutExpo",
-      duration: 950,
-      delay: (el, i) => 150*i
-    });
 }
-
-// _____________________________________________/ Title Animation \_____________________________________________ //
-
-
-
-
-
-
-// ____________________/ Redirect To Other Pages \____________________ //
-
-function redirect_Country(){
-    var body = document.querySelector("body");
-    body.classList.add("fadeout");
-    let tID = setTimeout(function () {
-        window.location.href = country_name + ".html";
-        window.clearTimeout(tID);
-    }, 1500);
-    return hide_country_info();
-}
-
-// ____________________/ Redirect To Other Pages \____________________ //
